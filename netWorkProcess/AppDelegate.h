1eadc2f158592949bdb4453b96414fdd0fccf7ec
giga <pennlin@pennlintekiMac-mini.local>
2017-10-09 14:16:01 +0000

    Initial Commit

---
+++ netWorkProcess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netWorkProcess
+//
+//  Created by giga on 2017/10/9.
+//  Copyright © 2017年 深圳市齐家互联网科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ netWorkProcess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netWorkProcess
+//
+//  Created by giga on 2017/10/9.
+//  Copyright © 2017年 深圳市齐家互联网科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
